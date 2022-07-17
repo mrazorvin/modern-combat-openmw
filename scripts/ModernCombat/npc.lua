3b93fdac2683f53836c106a5b146c2c94bbbf0ee
Mikhail Razorvin <mikhailr@ematiq.com>
2022-07-17 16:44:55 +0200

    feat(NPC): Stabilized NPC attributes adjustment

--- scripts/ModernCombat/npc.lua
+++ scripts/ModernCombat/npc.lua
@@ -3,211 +3,284 @@ local self = require('openmw.self')
 local core = require('openmw.core')
 local storage = require('openmw.storage')
 local types = require('openmw.types')
+local ai = require('openmw.interfaces').AI
 
+-- @settings
 local MOD_NAME = "ModernCombat"
 local MOD_SETTINGS = MOD_NAME .. "Settings__"
-
 local settings = storage.globalSection(MOD_SETTINGS)
 
-local is_stats_adjusted = false
-local is_magick_skill_adjusted = false
+-- @permanent
+local os_conjuration = 0
+local os_alteration = 0
+local os_illusion = 0
+local os_mysticism = 0
+local os_restoration = 0
+local os_destruction = 0
 
-local origin_conjuration = 0
-local origin_alteration = 0
-local origin_illusion = 0
-local origin_mystic = 0
-local origin_restoration = 0
-local origin_destruction = 0
-local origin_athletics = 0
+local os_longblade = 0
+local os_shortblade = 0
+local os_spear = 0
+local os_axe = 0
+local os_bluntweapon = 0
+local os_marksman = 0
 
-local origin_luck = 0
-local origin_speed = 0
+local os_athletics = 0
 
+local oa_luck = 0
+local oa_speed = 0
+
+local is_actor_affected = false
+local is_snapshot_created = false
+
+-- @temporary
 local is_creature = types.Creature.objectIsInstance(self)
 local is_humanoid = not is_creature
 
-local throttle_offensive_spellcasting_timer = 0
+local low_fatigue_timer = 0
+local low_magicka_timer = 0
+local prev_magicka = 0
 
-time.runRepeatedly(function()
+function on_update(is_inactive)
+    local target = ai.getActiveTarget('Combat') or ai.getActiveTarget('Pursue')
     local is_mod_disabled = settings:get("Disabled")
     local is_mod_enabled = not is_mod_disabled
+    local is_script_active = not is_inactive
+    local is_script_inactive = not is_script_active
+    local is_in_combat = target ~= nil
 
-    local magicka = types.NPC.stats.dynamic.magicka(self)
-
-    local is_magick_user = magicka.current / magicka.base < 0.25
-    local is_not_started = throttle_offensive_spellcasting_timer == 0
-    if is_mod_enabled and is_magick_user and is_not_started then
-        throttle_offensive_spellcasting_timer = 8
-        adjust_magick_skills()
-    end
-
-    if is_mod_enabled and throttle_offensive_spellcasting_timer ~= 0 then
-        magicka.current = magicka.base
-
-        throttle_offensive_spellcasting_timer = throttle_offensive_spellcasting_timer - 2
-        if throttle_offensive_spellcasting_timer == 0 then
-            restore_magick_skills()
-        end
-    elseif is_mod_enabled and magicka.current ~= magicka.base then
-        magicka.current = magicka.current - magicka.current * 0.1
-    end
-
-    if is_mod_enabled and not is_stats_adjusted then
-        adjust_stats()
-    elseif is_mod_disabled and is_stats_adjusted then
-        restore_stats()
-    end
-end, 2 * time.second)
-
-function adjust_stats()
-    if is_stats_adjusted then
+    local skip_update = not is_actor_affected and (not is_in_combat or is_mod_disabled)
+    if skip_update then
         return;
     else
-        is_stats_adjusted = true
+        is_actor_affected = true
     end
 
-    local luck = types.NPC.stats.attributes.luck(self)
-    origin_luck = luck.base
-    if luck.base < 1000 then
-        luck.base = 1000
+    local skills = types.NPC.stats.skills
+    local attributes = types.NPC.stats.attributes
+
+    local s_conjuration = skills.conjuration(self)
+    local s_alteration = skills.alteration(self)
+    local s_illusion = skills.illusion(self)
+    local s_mysticism = skills.mysticism(self)
+    local s_restoration = skills.restoration(self)
+    local s_destruction = skills.destruction(self)
+
+    local s_longblade = skills.longblade(self)
+    local s_shortblade = skills.shortblade(self)
+    local s_spear = skills.spear(self)
+    local s_axe = skills.axe(self)
+    local s_bluntweapon = skills.bluntweapon(self)
+    local s_marksman = skills.marksman(self)
+
+    local s_athletics = skills.athletics(self)
+
+    local a_speed = attributes.speed(self)
+    local a_luck = attributes.luck(self)
+
+    -- snapshot existed attributes
+    if not is_snapshot_created then
+        is_snapshot_created = true
+
+        oa_speed = a_speed.base
+        oa_luck = a_luck.base
+
+        -- creature doesn't has attributes
+        if is_creature then
+            return
+        end
+
+        os_conjuration = s_conjuration.base
+        os_alteration = s_alteration.base
+        os_illusion = s_illusion.base
+        os_mysticism = s_mysticism.base
+        os_restoration = s_restoration.base
+        os_destruction = s_destruction.base
+
+        os_longblade = s_longblade.base
+        os_shortblade = s_shortblade.base
+        os_spear = s_spear.base
+        os_axe = s_axe.base
+        os_bluntweapon = s_bluntweapon.base
+        os_marksman = s_marksman.base
+
+        os_athletics = s_athletics.base
+
+        return
     end
 
-    local speed = types.NPC.stats.attributes.speed(self)
-    if is_humanoid then
-        local athletics = types.NPC.stats.skills.athletics(self)
+    -- restore existed attributes
+    if is_snapshot_created and (is_mod_disabled or is_script_inactive or not is_in_combat) then
+        is_snapshot_created = false
+        is_actor_affected = false
 
-        origin_athletics = athletics.base
-        if athletics.base < 70 then
-            athletics.base = 70
+        a_speed.base = oa_speed
+        a_luck.base = oa_luck
+
+        -- creature doesn't has attributes
+        if is_creature then
+            return
         end
 
-        origin_speed = speed.base
-        if speed.base < 50 then
-            speed.base = 50
-        end
-    elseif is_creature then
-        origin_speed = speed.base
-        if speed.base < 10 then
-            speed.base = 10
-        end
+        s_conjuration.base = os_conjuration
+        s_alteration.base = os_alteration
+        s_illusion.base = os_illusion
+        s_mysticism.base = os_mysticism
+        s_restoration.base = os_restoration
+        s_destruction.base = os_destruction
+
+        s_longblade.base = os_longblade
+        s_shortblade.base = os_shortblade
+        s_spear.base = os_spear
+        s_axe.base = os_axe
+        s_bluntweapon.base = os_bluntweapon
+        s_marksman.base = os_marksman
+
+        s_athletics.base = os_athletics
+
+        return
     end
-end
 
-function restore_stats()
-    if not is_stats_adjusted then
+    -- @stats
+    local d_magicka = types.NPC.stats.dynamic.magicka(self)
+    local d_fatigue = types.NPC.stats.dynamic.fatigue(self)
+
+    -- @modifiers
+    local mod_destruction = 1
+
+    -- @magicka
+    local is_magick_user = d_magicka.current / d_magicka.base < 0.25
+    local is_low_magick_active = low_magicka_timer ~= 0
+    local is_low_magick_disabled = low_magicka_timer == 0
+    if is_magick_user and is_low_magick_disabled then
+        low_magicka_timer = 4
+    end
+
+    if is_low_magick_active then
+        low_magicka_timer = low_magicka_timer - 1
+    end
+
+    if is_low_magick_active then
+        mod_destruction = 0
+        d_magicka.current = d_magicka.current + d_magicka.base * 0.25
+    elseif d_magicka.current ~= prev_magicka then
+        d_magicka.current = d_magicka.current - d_magicka.current * 0.1
+    end
+    prev_magicka = d_magicka.current
+
+    -- @fatigue
+    local is_low_fatigue = d_fatigue.current / d_fatigue.base < 0.05
+    if is_low_fatigue then
+        low_fatigue_timer = low_fatigue_timer + 1
+    end
+    if is_low_fatigue == 8 then
+        low_fatigue_timer = 0
+        d_fatigue.current = d_fatigue.base
+    end
+
+    -- @creature
+    if is_creature then
+        if a_luck.base < 1000 then
+            a_luck.base = 1000
+        end
+
+        if a_speed.base < 10 then
+            a_speed.base = 10
+        end
+
         return;
-    else
-        is_stats_adjusted = false
     end
 
-    local is_humanoid = not types.Creature.objectIsInstance(self)
-    if is_humanoid then
-        local athletics = types.NPC.stats.skills.athletics(self)
-        athletics.base = origin_athletics
-        restore_magick_skills()
+    -- @humanoid
+    if s_athletics.base < 70 then
+        s_athletics.base = 70
     end
 
-    local speed = types.NPC.stats.attributes.speed(self)
-    local luck = types.NPC.stats.attributes.luck(self)
+    if a_speed.base < 50 then
+        a_speed.base = 50
+    end
 
-    luck.base = origin_luck
-    speed.base = origin_speed
+    s_conjuration.base = os_conjuration + 100
+    s_alteration.base = os_alteration + 100
+    s_illusion.base = os_illusion + 100
+    s_mysticism.base = os_mysticism + 100
+    s_restoration.base = os_restoration + 100
+    s_destruction.base = (os_destruction + 100) * mod_destruction
+
+    s_longblade.base = os_longblade + 100
+    s_shortblade.base = os_shortblade + 100
+    s_spear.base = os_spear + 100
+    s_axe.base = os_axe + 100
+    s_bluntweapon.base = os_bluntweapon + 100
+    s_marksman.base = os_marksman + 100
 end
 
-function restore_magick_skills()
-    if not is_magick_skill_adjusted then
-        return
-    else
-        is_magick_skill_adjusted = false
-    end
-
-    local conjuration = types.NPC.stats.skills.conjuration(self)
-    local alteration = types.NPC.stats.skills.alteration(self)
-    local illusion = types.NPC.stats.skills.illusion(self)
-    local mysticism = types.NPC.stats.skills.mysticism(self)
-    local restoration = types.NPC.stats.skills.restoration(self)
-    local destruction = types.NPC.stats.skills.destruction(self)
-    local athletics = types.NPC.stats.skills.athletics(self)
-
-    conjuration.base = origin_conjuration
-    alteration.base = origin_alteration
-    illusion.base = origin_illusion
-    mysticism.base = origin_mystic
-    restoration.base = origin_restoration
-    destruction.base = origin_destruction
-end
-
-function adjust_magick_skills()
-    if is_magick_skill_adjusted then
-        return
-    else
-        is_magick_skill_adjusted = true
-    end
-
-    local conjuration = types.NPC.stats.skills.conjuration(self)
-    local alteration = types.NPC.stats.skills.alteration(self)
-    local illusion = types.NPC.stats.skills.illusion(self)
-    local mysticism = types.NPC.stats.skills.mysticism(self)
-    local restoration = types.NPC.stats.skills.restoration(self)
-    local destruction = types.NPC.stats.skills.destruction(self)
-
-    origin_conjuration = conjuration.base
-    origin_alteration = alteration.base
-    origin_illusion = illusion.base
-    origin_mystic = mysticism.base
-    origin_restoration = restoration.base
-    origin_destruction = destruction.base
-
-    conjuration.base = 1000
-    alteration.base = 1000
-    illusion.base = 1000
-    mysticism.base = 1000
-    restoration.base = 1000
-
-    -- offensive skills suprresion
-    destruction.base = -1000
-end
-
+local last_update_time = core.getRealTime()
 return {
     engineHandlers = {
         onInactive = function()
+            on_update(true)
             core.sendGlobalEvent('ActorInactive', {self.object})
-            restore_stats()
         end,
         onActive = function()
-            adjust_stats()
+            on_update(false)
+        end,
+        onUpdate = function()
+            local real_time = core.getRealTime()
+            local seconds_passed_since_update = real_time - last_update_time
+
+            if seconds_passed_since_update > 2 then
+                on_update(false)
+                last_update_time = real_time
+            end
         end,
         onSave = function()
             return {
-                is_stats_adjusted = is_stats_adjusted,
-                is_magick_skill_adjusted = is_magick_skill_adjusted,
+                os_conjuration = os_conjuration,
+                os_alteration = os_alteration,
+                os_illusion = os_illusion,
+                os_mysticism = os_mysticism,
+                os_restoration = os_restoration,
+                os_destruction = os_destruction,
 
-                origin_conjuration = origin_conjuration,
-                origin_alteration = origin_alteration,
-                origin_illusion = origin_illusion,
-                origin_mystic = origin_mystic,
-                origin_restoration = origin_restoration,
-                origin_destruction = origin_destruction,
-                origin_athletics = origin_athletics,
+                os_longblade = os_longblade,
+                os_shortblade = os_shortblade,
+                os_spear = os_spear,
+                os_axe = os_axe,
+                os_bluntweapon = os_bluntweapon,
+                os_marksman = os_marksman,
 
-                origin_luck = origin_luck,
-                origin_speed = origin_speed
+                os_athletics = os_athletics,
+
+                oa_luck = oa_luck,
+                oa_speed = oa_speed,
+
+                is_actor_affected = is_actor_affected,
+                is_snapshot_created = is_snapshot_created
             }
         end,
         onLoad = function(data)
-            is_stats_adjusted = data.is_stats_adjusted
-            is_magick_skill_adjusted = data.is_magick_skill_adjusted
+            os_conjuration = data.os_conjuration
+            os_alteration = data.os_alteration
+            os_illusion = data.os_illusion
+            os_mysticism = data.os_mysticism
+            os_restoration = data.os_restoration
+            os_destruction = data.os_destruction
 
-            origin_conjuration = data.origin_conjuration
-            origin_alteration = data.origin_alteration
-            origin_illusion = data.origin_illusion
-            origin_mystic = data.origin_mystic
-            origin_restoration = data.origin_restoration
-            origin_destruction = data.origin_destruction
-            origin_athletics = data.origin_athletics
+            os_longblade = data.os_longblade
+            os_shortblade = data.os_shortblade
+            os_spear = data.os_spear
+            os_axe = data.os_axe
+            os_bluntweapon = data.os_bluntweapon
+            os_marksman = data.os_marksman
 
-            origin_luck = data.origin_luck
-            origin_speed = data.origin_speed
+            os_athletics = data.os_athletics
+
+            oa_luck = data.oa_luck
+            oa_speed = data.oa_speed
+
+            is_actor_affected = data.is_actor_affected
+            is_snapshot_created = data.is_snapshot_created
         end
     }
 }
